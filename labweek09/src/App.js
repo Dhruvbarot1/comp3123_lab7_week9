cef55382fa76b39921f4c93ff0c11715dfc0c940
Dhruv Barot <101415874@georgebrown.ca>
2024-11-03 21:46:56 -0500

    Initial commit for lab 9 modifications

---
+++ labweek09/src/App.js
@@ -0,0 +1,24 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" 
+          className="App-logo" 
+          alt="React Logo" 
+        />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <p>React JS Programming Week09 Lab exercise</p>
+        <p>101415874</p>
+        <p>Dhruv Barot</p>
+        <p>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
